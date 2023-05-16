d801d86cca7d5834127178484bea2a85b3320c1a
amaralc <amaral.calil@gmail.com>
2023-05-15 21:37:35 -0300

    config: format

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', async (request, reply) => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on http://localhost:3333')
+  })
